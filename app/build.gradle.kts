4f8e35b714b03fb2d04e8afcc0dd00dad5541a17
markusingierd <markus.ingierd@gmail.com>
2025-02-11 16:31:42 +0100

    fikser warning i dependencies og gjort karetet mindre på TrailScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,11 +41,11 @@ android {
 
 dependencies {
 
-    implementation("com.mapbox.maps:android:11.9.2")
-    implementation("com.mapbox.extension:maps-compose:11.9.2")
-    implementation("androidx.annotation:annotation:1.0.0")
-    implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation(libs.mapbox)
+    implementation(libs.maps.compose)
+    implementation(libs.androidx.annotation)
+    implementation(libs.play.services.location)
+    implementation(libs.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/no/usn/bop3000/ui/screens/TrailScreen.kt
+++ app/src/main/java/no/usn/bop3000/ui/screens/TrailScreen.kt
@@ -6,18 +6,32 @@ import androidx.compose.ui.Modifier
 import com.mapbox.geojson.Point
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip
 
 @Composable
 fun TrailScreen() {
-    MapboxMap(
-        Modifier.fillMaxSize(),
-        mapViewportState = rememberMapViewportState {
-            setCameraOptions {
-                zoom(2.0)
-                center(Point.fromLngLat(-98.0, 39.5))
-                pitch(0.0)
-                bearing(0.0)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        MapboxMap(
+            modifier = Modifier
+                .width(300.dp)
+                .height(400.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            mapViewportState = rememberMapViewportState {
+                setCameraOptions {
+                    zoom(5.0)
+                    center(Point.fromLngLat(-98.0, 39.5))
+                }
             }
-        },
-    )
+        )
+    }
 }
+
